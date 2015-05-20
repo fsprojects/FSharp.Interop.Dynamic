e8cc983c298d0654580e6c0f03a637fc25b9e598
jbtule <jay+code@tuley.name>
2015-05-20 11:52:29 -0500

    Updated nunit runner

--- .ci/nunit.sh
+++ .ci/nunit.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -x
 
-mono --runtime=v4.0 .nuget/NuGet.exe install NUnit.Runners -Version 2.6.1 -o packages
+mono --runtime=v4.0 .nuget/NuGet.exe install NUnit.Runners -Version 2.6.4 -o packages
 
 runTest(){
-    mono --runtime=v4.0 packages/NUnit.Runners.2.6.1/tools/nunit-console.exe -noxml -nodots -labels -stoponerror $@
+    mono --runtime=v4.0 packages/NUnit.Runners.2.6.4/tools/nunit-console.exe -noxml -nodots -labels -stoponerror $@
    if [ $? -ne 0 ]
    then   
      exit 1
