cdc36b8e6f401cc4180a23696bf74646ebe357c8
Jay Tuley <jay@tuley.name>
2017-07-05 16:16:13 -0500

    15.0 msbuild hint

--- build.fsx
+++ build.fsx
@@ -31,7 +31,7 @@ let commonBuild target =
     let buildMode = getBuildParamOrDefault "buildMode" "Release"
     let setParams defaults =
             { defaults with
-                ToolsVersion = Some("15.3")
+                ToolsVersion = Some("15.0")
                 Verbosity = Some(Quiet)
                 Targets = [target]
                 Properties =
