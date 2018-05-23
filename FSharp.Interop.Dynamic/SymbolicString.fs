fa1b79ee1092391e05be079996d48335ca2fbad7
jbtule <jay+code@tuley.name>
2018-05-23 15:51:41 -0500

    get type as well not just name of

--- FSharp.Interop.Dynamic/SymbolicString.fs
+++ FSharp.Interop.Dynamic/SymbolicString.fs
@@ -8,16 +8,22 @@ module SymbolicString =
     let sym<'TTarget> : 'TTarget = failwith "don't call the sym function, meant for quotations only!"
     
     
-    type Name =
-        static member Of([<ReflectedDefinition>] value:Expr<'T>) : string =
-            let rec finalName value' =
-                match value' with
-                    | ValueWithName(_, _, name) -> name
-                    | NewUnionCase(caseInfo, _) -> caseInfo.Name
-                    | PropertyGet(_, propOrValInfo, _) -> propOrValInfo.Name
-                    | FieldGet(_, fieldInfo) -> fieldInfo.Name
-                    | Call(_, methInfo, _) -> methInfo.Name
-                    | Lambda(_, expr) -> finalName expr
-                    | Let(_,_, expr) -> finalName expr
-                    | ________________________________ -> invalidArg "value" (sprintf "Couldn't figure out how to make '%A' a name" value)
-            finalName value
+    type Symbol =
+
+        static member nameWithTypeOf([<ReflectedDefinition>] value:Expr<'T>) : string * System.Type =
+                    let rec finalName value' =
+                        match value' with
+                            | ValueWithName(_, type', name) -> name, type'
+                            | NewUnionCase(caseInfo, _) -> caseInfo.Name, caseInfo.DeclaringType
+                            | PropertyGet(_, propOrValInfo, _) -> propOrValInfo.Name, propOrValInfo.PropertyType
+                            | FieldGet(_, fieldInfo) -> fieldInfo.Name, fieldInfo.FieldType
+                            | Call(_, methInfo, _) -> methInfo.Name, methInfo.ReturnType
+                            | Lambda(_, expr) -> finalName expr
+                            | Let(_,_, expr) -> finalName expr
+                            | ________________________________ -> invalidArg "value" (sprintf "Couldn't figure out how to make '%A' a name" value)
+                    finalName value
+                    
+        static member nameOf([<ReflectedDefinition>] value:Expr<'T>) : string = value |> Symbol.nameWithTypeOf |> fst
+              
+        static member typeOf([<ReflectedDefinition>] value:Expr<'T>) : System.Type = value |> Symbol.nameWithTypeOf |> snd
+                     
--- Tests/Raw.fs
+++ Tests/Raw.fs
@@ -7,49 +7,58 @@ open FSharp.Interop.Dynamic.SymbolicString
 open Xunit
 open FsUnit.Xunit
 open System.Linq
+open System
+open System.Collections.Generic
 
 module Raw =
+    open System.Collections
 
-   [<Fact>] 
+    [<Fact>] 
     let ``Name of Call`` ()=
-        Name.Of(sym<string>.Substring(0,0)) |> should equal "Substring"
+        Symbol.nameOf(sym<string>.Substring(0,0)) |> should equal "Substring"
+        Symbol.typeOf(sym<string>.Substring(0,0)) |> should equal typeof<string>
   
     [<Fact>]
     let ``Name of unapplied method`` ()=
-        Name.Of(sym<string>.Substring) |> should equal "Substring"
+        Symbol.nameOf(sym<string>.Substring) |> should equal "Substring"
+        Symbol.typeOf(sym<string>.Substring) |> should equal typeof<string>
     
     [<Fact>]
     let ``Name of get`` ()=
-        Name.Of(sym<string>.Length) |> should equal "Length"
+        Symbol.nameOf(sym<string>.Length) |> should equal "Length"
+        Symbol.typeOf(sym<string>.Length) |> should equal typeof<int>
     
     [<Fact>]
     let ``Name of end of chain`` ()=
-        Name.Of(sym<string>.Length.CompareTo) |> should equal "CompareTo"
+        Symbol.nameOf(sym<string>.Length.CompareTo) |> should equal "CompareTo"
+        Symbol.typeOf(sym<string>.Length.CompareTo) |> should equal typeof<int>
     
     [<Fact>]
     let ``Name of End of chain overload`` ()=
-        Name.Of(sym<string>.Length.ToString(sym<string>)) |> should equal "ToString"
+        Symbol.nameOf(sym<string>.Length.ToString(sym<string>)) |> should equal "ToString"
+        Symbol.typeOf(sym<string>.Length.ToString(sym<string>)) |> should equal typeof<string>
 
     [<Fact>]
     let ``Name of End of chain mix and match extension`` ()=
-        Name.Of(sym<string>.Length.ToString(sym<string>).Length.ToString().Any) |> should equal "Any"
- 
+        Symbol.nameOf(sym<string>.Length.ToString(sym<string>).Length.ToString().Any) |> should equal "Any"
+        Symbol.typeOf(sym<string>.Length.ToString(sym<string>).Length.ToString().Any) |> should equal typeof<bool>
     [<Fact>]
     let ``Name of var`` ()=
         let x = 0
-        Name.Of(x) |> should equal "x"
+        Symbol.nameOf(x) |> should equal "x"
+        Symbol.typeOf(x) |> should equal typeof<int>
 
     [<Fact>]
     let ``Name of static generic member`` ()=
-        Name.Of(Enumerable.Empty) |> should equal "Empty"
-    
+        Symbol.nameOf(Enumerable.Empty) |> should equal "Empty"
+        Symbol.typeOf(Enumerable.Empty) |> should equal typeof<IEnumerable<_>>
     [<Fact>]
     let ``Name of DU`` ()=
         let x = 0
-        Name.Of(Option.Some) |> should equal "Some"
-
+        Symbol.nameOf(Option.Some) |> should equal "Some"
+        Symbol.typeOf(Option.Some) |> should equal typeof<Option<_>>
     [<Fact>]
     let ``Call method off of an object dynamically`` ()=
-        let name = Name.Of(sym<string>.Substring(0,0))
+        let name = Symbol.nameOf(sym<string>.Substring(0,0))
         let actual = "HelloWorld" |> Dyn.invokeMember name (0,5) 
         actual |> should equal "Hello"
